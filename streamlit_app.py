694ca3cafb91ff31d805714055fe3017dcb16aa8
Keisuke Magara <61485115+kei-mag@users.noreply.github.com>
2024-08-12 10:33:52 +0900

    Create streamlit_app.py
    Add some test codes.

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.DataFrame({'col1': [1,2,3]})
+df
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig
+
+selection = "<not selected>"
+selection = st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
+'You choose ', selection, '.'
+
+x = 10
+'x: ', x 
